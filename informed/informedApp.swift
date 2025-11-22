3b1762571779c5365df6e08b8e4d8068856b309e
robert ryan <jacobrryan1@gmail.com>
2025-11-22 21:20:31 +0200

    Initial Commit

---
+++ informed/informedApp.swift
@@ -0,0 +1,17 @@
+//
+//  informedApp.swift
+//  informed
+//
+//  Created by Jacob Ryan on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct informedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
